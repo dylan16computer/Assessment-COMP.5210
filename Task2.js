f251e7526e620384899aa227764f077fb525d1c7
Dylan Hodder <30002799@student.toiohomai.ac.nz>
2018-09-21 11:09:48 +1200

    Assessment complete

--- Task2.js
+++ Task2.js
@@ -1,28 +1,3 @@
-// function PresetValues()
-// {
-//     var volume = [5000, 5500, 6300, 9000, 6000, 5000];
-//     var sharePrice = [0.55, 0.58, 0.6, 0.63, 0.65, 0.7];
-//     var totalCost = 0;
-//     var totalVolume = 0;
-//     var movAvg = 0;
-//     for(j = 0; j < volume.length; j++)
-//     {
-//         var transCost = volume[j] * sharePrice[j];
-//         totalCost += transCost;
-//         totalVolume += volume[j];
-//         movAvg = totalCost / totalVolume;
-//         var profLoss = 100 - (100 * (movAvg / sharePrice[j]));
-//         document.getElementById(`row${j+2}col1`).innerHTML = volume[j];
-//         document.getElementById(`row${j+2}col2`).innerHTML = sharePrice[j];
-//         document.getElementById(`row${j+2}col3`).innerHTML = `$${transCost}`;
-//         document.getElementById(`row${j+2}col4`).innerHTML = `$${totalCost}`;
-//         document.getElementById(`row${j+2}col5`).innerHTML = totalVolume;
-//         document.getElementById(`row${j+2}col6`).innerHTML = movAvg.toFixed(4);
-//         document.getElementById(`row${j+2}col7`).innerHTML = profLoss.toFixed(4);
-//     }
-//     
-// }
-
 function Calc()
 {
     var volume = [];
@@ -31,7 +6,7 @@ function Calc()
     var totalVolume = 0;
     for(let i = 0; i < 6; i++)
     {
-        volume[i] = document.getElementById(`vol${i+1}`).value;
+        volume[i] = Number(document.getElementById(`vol${i+1}`).value);
         sharePrice[i] = document.getElementById(`pri${i+1}`).value;
         var transCost = volume[i] * sharePrice[i];
         totalCost += transCost;
