377e464271dd3e93719352e70160f8c2953f933a
Dylan Hodder <30002799@student.toiohomai.ac.nz>
2018-09-21 10:20:13 +1200

    Working on ClearTable function and need to code calculations

--- Task2.js
+++ Task2.js
@@ -1,36 +1,67 @@
-function presetValues()
+// function PresetValues()
+// {
+//     var volume = [5000, 5500, 6300, 9000, 6000, 5000];
+//     var sharePrice = [0.55, 0.58, 0.6, 0.63, 0.65, 0.7];
+//     var totalCost = 0;
+//     var totalVolume = 0;
+//     var movAvg = 0;
+//     for(j = 0; j < volume.length; j++)
+//     {
+//         var transCost = volume[j] * sharePrice[j];
+//         totalCost += transCost;
+//         totalVolume += volume[j];
+//         movAvg = totalCost / totalVolume;
+//         var profLoss = 100 - (100 * (movAvg / sharePrice[j]));
+//         document.getElementById(`row${j+2}col1`).innerHTML = volume[j];
+//         document.getElementById(`row${j+2}col2`).innerHTML = sharePrice[j];
+//         document.getElementById(`row${j+2}col3`).innerHTML = `$${transCost}`;
+//         document.getElementById(`row${j+2}col4`).innerHTML = `$${totalCost}`;
+//         document.getElementById(`row${j+2}col5`).innerHTML = totalVolume;
+//         document.getElementById(`row${j+2}col6`).innerHTML = movAvg.toFixed(4);
+//         document.getElementById(`row${j+2}col7`).innerHTML = profLoss.toFixed(4);
+//     }
+//     document.getElementById(`row2col6`).innerHTML = "";
+//     document.getElementById(`row2col7`).innerHTML = "";
+// }
+
+function Calc()
 {
-    var volume = [5000, 5500, 6300, 9000, 6000, 5000];
-    var sharePrice = [0.55, 0.58, 0.6, 0.63, 0.65, 0.7];
+    var volume = [];
+    var sharePrice = [];
     var totalCost = 0;
     var totalVolume = 0;
-    var movAvg = 0;
-    for(j = 0; j < volume.length; j++)
+    for(let t = 0; t < 6; t++)
     {
-        var transCost = volume[j] * sharePrice[j];
+        volume[t] = document.getElementById(`vol${t+1}`).value;
+        sharePrice[t] = document.getElementById(`pri${t+1}`).value;
+        var transCost = volume[t] * sharePrice[t];
         totalCost += transCost;
-        totalVolume += volume[j];
+        totalVolume += volume[t];
         movAvg = totalCost / totalVolume;
-        var profLoss = 100 - (100 * (movAvg / sharePrice[j]));
-        document.getElementById(`row${j+2}col1`).innerHTML = volume[j];
-        document.getElementById(`row${j+2}col2`).innerHTML = sharePrice[j];
-        document.getElementById(`row${j+2}col3`).innerHTML = `$${transCost}`;
-        document.getElementById(`row${j+2}col4`).innerHTML = `$${totalCost}`;
-        document.getElementById(`row${j+2}col5`).innerHTML = totalVolume;
-        document.getElementById(`row${j+2}col6`).innerHTML = movAvg.toFixed(4);
-        document.getElementById(`row${j+2}col7`).innerHTML = profLoss.toFixed(4);
-    }
-        document.getElementById(`row2col6`).innerHTML = "";
-        document.getElementById(`row2col7`).innerHTML = "";
+        var profLoss = 100 - (100 * (movAvg / sharePrice[t]));
+        document.getElementById(`row${t+2}col3`).innerHTML = `$${transCost}`;
+        document.getElementById(`row${t+2}col4`).innerHTML = `$${totalCost}`;
+        document.getElementById(`row${t+2}col5`).innerHTML = totalVolume;
+        document.getElementById(`row${t+2}col6`).innerHTML = movAvg.toFixed(4);
+        document.getElementById(`row${t+2}col7`).innerHTML = profLoss.toFixed(4);
+    } 
 }
 
-function calc()
+function ClearTable()
 {
-    var volume = [5000, 5500, 6300, 9000, 6000, 5000];
-    var sharePrice = [0.55, 0.58, 0.6, 0.63, 0.65, 0.7];
-    for(i = 0; i < volume.length; i++)
+    for(q = 1; q < 7; q++)
     {
-        var transCost = Number(volume[i] * sharePrice[i]);
-        document.getElementById(`row${i+2}col3`).innerHTML = "$" + transCost;
+        document.getElementById(`vol${q}`).innerHTML = "";
+        document.getElementById(`vol${q}`).innerHTML = "";
+        document.getElementById(`vol${q}`).innerHTML = "";
+        document.getElementById(`vol${q}`).innerHTML = "";
+        document.getElementById(`vol${q}`).innerHTML = "";
+        document.getElementById(`vol${q}`).innerHTML = "";
+        document.getElementById(`pri${q}`).innerHTML = "";
+        document.getElementById(`pri${q}`).innerHTML = "";
+        document.getElementById(`pri${q}`).innerHTML = "";
+        document.getElementById(`pri${q}`).innerHTML = "";
+        document.getElementById(`pri${q}`).innerHTML = "";
+        document.getElementById(`pri${q}`).innerHTML = "";
     }
 }
